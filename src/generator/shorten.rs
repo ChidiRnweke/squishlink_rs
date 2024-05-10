e9f83fd3a29fb53f0aa72d864d9647bae9e97aae
ChidiRNweke <chidi125@gmail.com>
2024-05-10 20:42:29 +0200

    Complete shorten service

--- src/generator/shorten.rs
+++ src/generator/shorten.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use super::name_generator::{GeneratedName, NameGeneratorTrait};
 use url::Url;
 
@@ -7,12 +9,20 @@ pub trait NamesRepository {
     fn retrieve_original_name(&self, name: &GeneratedName) -> Result<String, String>;
 }
 
+pub struct OutputLink(pub String);
+
+impl fmt::Display for OutputLink {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 pub trait Shortener {
     fn shorten_name(
         &self,
-        name: &Url,
+        name: &str,
         rng: &mut rand::rngs::ThreadRng,
-    ) -> Result<GeneratedName, String>;
+    ) -> Result<OutputLink, String>;
 }
 
 pub struct LinkShortener<T, B>
@@ -20,6 +30,7 @@ where
     T: NamesRepository,
     B: NameGeneratorTrait,
 {
+    base_url: String,
     names_repo: T,
     generator: B,
 }
@@ -28,8 +39,9 @@ where
     T: NamesRepository,
     B: NameGeneratorTrait,
 {
-    pub fn new(names_repo: T, generator: B) -> Self {
+    pub fn new(base_url: String, names_repo: T, generator: B) -> Self {
         Self {
+            base_url,
             names_repo,
             generator,
         }
@@ -42,6 +54,10 @@ where
             Err(_) => Err("Invalid URL".to_string()),
         }
     }
+
+    fn to_output_link(&self, generated_name: &GeneratedName) -> OutputLink {
+        OutputLink(self.base_url.clone() + "/" + &generated_name.0)
+    }
 }
 
 impl<T, B> Shortener for LinkShortener<T, B>
@@ -51,9 +67,10 @@ where
 {
     fn shorten_name(
         &self,
-        input: &Url,
+        input: &str,
         rng: &mut rand::rngs::ThreadRng,
-    ) -> Result<GeneratedName, String> {
+    ) -> Result<OutputLink, String> {
+        let validated_input = self.validate_input(input)?;
         let mut generated_name = self.generator.make_random_name(rng);
         let mut already_exists = self.names_repo.name_exists(&generated_name)?;
         loop {
@@ -63,7 +80,8 @@ where
             generated_name = self.generator.make_random_name(rng);
             already_exists = self.names_repo.name_exists(&generated_name)?;
         }
-        self.names_repo.store_name(input, &generated_name)?;
-        Ok(generated_name)
+        self.names_repo
+            .store_name(&validated_input, &generated_name)?;
+        Ok(self.to_output_link(&generated_name))
     }
 }
