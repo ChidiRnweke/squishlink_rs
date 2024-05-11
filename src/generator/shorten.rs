d73a592b625890ccc558a036c5b7ad7bc1d86ff3
ChidiRNweke <chidi125@gmail.com>
2024-05-11 09:18:21 +0200

    Add tests to link shortener

--- src/generator/shorten.rs
+++ src/generator/shorten.rs
@@ -49,10 +49,7 @@ where
 
     fn validate_input(&self, input_link: &str) -> Result<Url, String> {
         let url = Url::parse(input_link);
-        match url {
-            Ok(valid_link) => Ok(valid_link),
-            Err(_) => Err("Invalid URL".to_string()),
-        }
+        url.map_err(|_| "Invalid URL".to_string())
     }
 
     fn to_output_link(&self, generated_name: &GeneratedName) -> OutputLink {
@@ -83,5 +80,77 @@ where
         self.names_repo
             .store_name(&validated_input, &generated_name)?;
         Ok(self.to_output_link(&generated_name))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::NameGenerator;
+    use rand::Rng;
+
+    struct MockNamesRepository {}
+    impl NamesRepository for MockNamesRepository {
+        fn store_name(&self, _original: &Url, _generated: &GeneratedName) -> Result<(), String> {
+            Ok(())
+        }
+
+        fn name_exists(&self, _name: &GeneratedName) -> Result<bool, String> {
+            let mut rng = rand::thread_rng();
+            let exists = rng.gen_bool(0.5);
+            Ok(exists)
+        }
+
+        fn retrieve_original_name(&self, _name: &GeneratedName) -> Result<String, String> {
+            Ok("".to_string())
+        }
+    }
+
+    #[test]
+    fn test_validate_input() {
+        let shortener = LinkShortener::new(
+            "http://localhost:8080".to_string(),
+            MockNamesRepository {},
+            NameGenerator::new(),
+        );
+        let result = shortener.validate_input("http://localhost:8080");
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_validate_input_invalid() {
+        let shortener = LinkShortener::new(
+            "http://localhost:8080".to_string(),
+            MockNamesRepository {},
+            NameGenerator::new(),
+        );
+        let result = shortener.validate_input("google.com");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_to_output_link() {
+        let shortener = LinkShortener::new(
+            "http://localhost:8080".to_string(),
+            MockNamesRepository {},
+            NameGenerator::new(),
+        );
+        let generated_name = GeneratedName("test".to_string());
+        let result = shortener.to_output_link(&generated_name);
+        assert_eq!(result.0, "http://localhost:8080/test");
+    }
+
+    #[test]
+    fn test_generating_link_does_not_hang() {
+        // This test is to ensure that the code does not hang when generating a link.
+        // This is because there is an infinite loop in the code.
+        let shortener = LinkShortener::new(
+            "http://localhost:8080".to_string(),
+            MockNamesRepository {},
+            NameGenerator::new(),
+        );
+        let mut rng = rand::thread_rng();
+        let result = shortener.shorten_name("http://localhost:8080", &mut rng);
+        assert!(result.is_ok());
     }
 }
