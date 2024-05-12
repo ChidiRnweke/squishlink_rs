685709c6da730d42250803345eba4efbfa5d93a3
ChidiRNweke <chidi125@gmail.com>
2024-05-12 12:07:19 +0200

    Remove unnecessary static lifetime from constants.
    This comes from a suggestion/linting rule from clippy

--- src/errors.rs
+++ src/errors.rs
@@ -9,11 +9,11 @@ pub enum AppError {
     NotFoundError,
 }
 
-const NOT_FOUND_ERR_MSG: &'static str =
+const NOT_FOUND_ERR_MSG: & str =
     "The resource you're looking for can't be found. Maybe it was already deleted? Links only stay valid for 7 days.";
-const DB_ERR_MSG: &'static str =
+const DB_ERR_MSG: &str =
     "An unexpected error occurred. If this persists please reach out and let me know.";
-const INPUT_ERR_MSG: &'static str  = "Something went wrong while trying to read your input. Is it a valid uri? (it must start with http:// or https://)";
+const INPUT_ERR_MSG: & str  = "Something went wrong while trying to read your input. Is it a valid uri? (it must start with http:// or https://)";
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
